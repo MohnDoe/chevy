6f6b245422293cb87cfbb7121d8bfb7627be8df8
MohnDoe <kfnpfeffer@gmail.com>
2025-09-06 14:33:44 +0200

    setting up neondb and prisma

---
+++ prisma/migrations/20250906123041_users/migration.sql
@@ -0,0 +1,21 @@
+-- CreateTable
+CREATE TABLE "public"."User" (
+    "id" TEXT NOT NULL,
+    "discordId" TEXT NOT NULL,
+    "hevyUsername" TEXT,
+    "hevyApiKey" TEXT,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "isVerifiedOnHevy" BOOLEAN NOT NULL DEFAULT false,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_discordId_key" ON "public"."User"("discordId");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_hevyUsername_key" ON "public"."User"("hevyUsername");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_hevyApiKey_key" ON "public"."User"("hevyApiKey");
