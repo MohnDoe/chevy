1e2ecbc016886f829db07a309c327d4616f23a30
MohnDoe <kfnpfeffer@gmail.com>
2025-09-18 17:25:49 +0200

    wip: reply to server command

---
+++ apps/bot/src/app/commands/(server)/server.tsx
@@ -0,0 +1,274 @@
+import {
+  ActionRow,
+  ButtonKit,
+  ChannelSelectMenu,
+  ChannelSelectMenuKit,
+  ChatInputCommandContext,
+  Command,
+  CommandData,
+  CommandMetadata,
+  Container,
+  Separator,
+  StringSelectMenu,
+  StringSelectMenuKit,
+  StringSelectMenuOption,
+  TextDisplay,
+} from "commandkit";
+import {
+  AnySelectMenuInteraction,
+  ApplicationCommandOptionType,
+  ApplicationCommandType,
+  ApplicationIntegrationType,
+  bold,
+  ButtonStyle,
+  ChannelSelectMenuInteraction,
+  ChannelType,
+  Colors,
+  InteractionContextType,
+  MessageFlags,
+  SeparatorSpacingSize,
+  StringSelectMenuInteraction,
+} from "discord.js";
+
+enum AutoShareWorkoutFormat {
+  Line,
+  BitDetailed,
+  FullDetails,
+}
+
+const AUTOSHARE_WORKOUT_FORMAT_LABELS: Record<AutoShareWorkoutFormat, string> =
+  {
+    [AutoShareWorkoutFormat.Line]: "Just a line",
+    [AutoShareWorkoutFormat.BitDetailed]: "Just some details",
+    [AutoShareWorkoutFormat.FullDetails]: "Full details!",
+  };
+
+export const metadata: CommandMetadata = {
+  userPermissions: ["ManageGuild"],
+};
+
+const _active = true;
+const _channelId = "1418237114470109376";
+const _mode = AutoShareWorkoutFormat.Line;
+const _participantCount = 23;
+
+export const command: CommandData = {
+  name: "server",
+  description: "Set-up Chevy on this server.",
+  contexts: [InteractionContextType.Guild],
+  integration_types: [ApplicationIntegrationType.GuildInstall],
+  type: ApplicationCommandType.ChatInput,
+  options: [
+    {
+      name: "auto-share",
+      description: "Chevy's auto-share feature !",
+      type: ApplicationCommandOptionType.SubcommandGroup,
+      options: [
+        {
+          name: "set",
+          description: "Configure auto-share feature.",
+          type: ApplicationCommandOptionType.Subcommand,
+          options: [
+            {
+              name: "active",
+              description:
+                "Should auto-share be active and sending new workouts?",
+              type: ApplicationCommandOptionType.Boolean,
+              required: true,
+            },
+            {
+              name: "channel",
+              description: "Destination channel",
+              type: ApplicationCommandOptionType.Channel,
+              required: true,
+            },
+            // {
+            //   name: "mode",
+            //   description: "Choose a frequency",
+            //   type: ApplicationCommandOptionType.String,
+            //   choices: [
+            //     { name: "Real-time", value: "real-time" },
+            //     { name: "Daily", value: "daily" },
+            //     { name: "Weekly", value: "real-time" },
+            //   ],
+            //   required: true,
+            // },
+            {
+              name: "format",
+              description: "What does the message look like?",
+              type: ApplicationCommandOptionType.String,
+              choices: Object.entries(AUTOSHARE_WORKOUT_FORMAT_LABELS).map(
+                ([name, value]) => ({
+                  name,
+                  value,
+                })
+              ),
+
+              required: true,
+            },
+            {
+              name: "mention",
+              description:
+                "Who should Chevy ping when sharing? (Leave empty to disable)",
+              type: ApplicationCommandOptionType.Role,
+            },
+          ],
+        },
+        {
+          name: "info",
+          description: "Explanations, current configuration and stats",
+          type: ApplicationCommandOptionType.Subcommand,
+        },
+      ],
+    },
+  ],
+};
+
+export async function chatInput({
+  interaction,
+  store,
+}: ChatInputCommandContext) {
+  await interaction.deferReply({
+    flags: MessageFlags.Ephemeral,
+  });
+  const subcommandGroup = interaction.options.getSubcommandGroup();
+  const subcommand = interaction.options.getSubcommand();
+
+  if (subcommandGroup == "auto-share") {
+    switch (subcommand) {
+      case "info":
+        await interaction.followUp({
+          flags: MessageFlags.Ephemeral | MessageFlags.IsComponentsV2,
+          components: settingsComponents(),
+        });
+        break;
+
+      case "set":
+        break;
+      default:
+        await interaction.followUp({
+          components: [<TextDisplay>Unknown command.</TextDisplay>],
+        });
+        break;
+    }
+    return;
+  }
+
+  await interaction.followUp({
+    components: [<TextDisplay>Unknown command.</TextDisplay>],
+  });
+}
+
+const handleSettingsChange = async (
+  interaction: StringSelectMenuInteraction | ChannelSelectMenuInteraction,
+  context: StringSelectMenuKit | ChannelSelectMenuKit
+) => {
+  await interaction.deferUpdate();
+  await interaction.editReply({
+    components: settingsComponents(),
+  });
+};
+
+const settingsComponents = () => {
+  return (
+    <>
+      <TextDisplay>
+        Auto-share allows Chevy to share automatically a workout whenever a
+        member completes their Hevy workout.
+      </TextDisplay>
+      <Separator spacing={SeparatorSpacingSize.Large} />
+      <TextDisplay>## Current settings</TextDisplay>
+      <Container accentColor={Colors.DarkButNotBlack}>
+        <TextDisplay content={`### Auto-share status`} />
+        <ActionRow>
+          <StringSelectMenu
+            customId={`autoShareStatusSelect---${new Date().toISOString()}`}
+            maxValues={1}
+            minValues={1}
+            onSelect={(i, c) =>
+              handleSettingsChange(
+                i as unknown as StringSelectMenuInteraction,
+                c
+              )
+            }
+            onEnd={console.log}
+          >
+            <StringSelectMenuOption
+              label="Active"
+              value="active"
+              default={_active}
+            />
+            <StringSelectMenuOption
+              label="Inactive"
+              value="nactive"
+              default={!_active}
+            />
+          </StringSelectMenu>
+        </ActionRow>
+        <Separator divider={false} />
+        <TextDisplay content={`### Destination channel`} />
+        <ActionRow>
+          <ChannelSelectMenu
+            customId={`autoShareChannelSelect---${new Date().toISOString()}`}
+            channelTypes={[ChannelType.GuildText]}
+            // defaultValues={[
+            //   {
+            //     type: SelectMenuDefaultValueType.Channel,
+            //     id: channelId,
+            //   },
+            // ]}
+            onSelect={(i, c) =>
+              handleSettingsChange(
+                i as unknown as ChannelSelectMenuInteraction,
+                c
+              )
+            }
+            onEnd={console.log}
+          />
+        </ActionRow>
+        <Separator divider={false} />
+        <TextDisplay content={`### Format`} />
+        <ActionRow>
+          <StringSelectMenu
+            customId={`autoShareFormatSelect---${new Date().toISOString()}`}
+            maxValues={1}
+            minValues={1}
+            onSelect={(i, c) =>
+              handleSettingsChange(
+                i as unknown as StringSelectMenuInteraction,
+                c
+              )
+            }
+            onEnd={console.log}
+          >
+            {Object.entries(AUTOSHARE_WORKOUT_FORMAT_LABELS).map(
+              ([key, value]) => (
+                <StringSelectMenuOption
+                  label={value}
+                  value={key}
+                  default={key == _mode.toString()}
+                />
+              )
+            )}
+          </StringSelectMenu>
+        </ActionRow>
+      </Container>
+      <TextDisplay>
+        {_participantCount} members are currently auto-sharing on this server.
+      </TextDisplay>
+      <ActionRow>
+        {new ButtonKit()
+          .setLabel("Save")
+          .setStyle(ButtonStyle.Primary)
+          .setCustomId("changeAutoShareSettings")
+          .onClick(() => {}, { once: true })}
+
+        {new ButtonKit()
+          .setLabel("Cancel")
+          .setStyle(ButtonStyle.Secondary)
+          .setCustomId("cancelAutoShareSettings")
+          .onClick(() => {}, { once: true })}
+      </ActionRow>
+    </>
+  );
+};
