bc52236c4034dda58bdb522712516b57d1abb853
doe <bonjour@doe.cool>
2025-10-14 15:41:17 +0200

    feat: small instruction text

--- apps/bot/src/app/commands/(LinkedUsers)/+auto-share.middleware.ts
+++ apps/bot/src/app/commands/(LinkedUsers)/+auto-share.middleware.ts
@@ -1,17 +1,24 @@
 import { getAutoShareConfig } from "@/features/core/server.service";
+import { commandMention } from "@/features/discord/command.service";
 import { Logger, stopMiddlewares, type MiddlewareContext } from "commandkit";
-import { ChatInputCommandInteraction, MessageFlags } from "discord.js";
+import { ChatInputCommandInteraction, MessageFlags, subtext } from "discord.js";
 
 export async function beforeExecute(ctx: MiddlewareContext) {
-    Logger.info('Checking if server has auto-share enabled.');
+  Logger.info("Checking if server has auto-share enabled.");
 
-    const serverAutoShareConfig = await getAutoShareConfig(ctx.interaction.guildId!);
+  const serverAutoShareConfig = await getAutoShareConfig(
+    ctx.interaction.guildId!,
+  );
 
-    if (!serverAutoShareConfig?.enabled) {
-        (ctx.interaction as unknown as ChatInputCommandInteraction).reply({
-            content: "Auto-share is not enabled on this server yet.",
-            flags: MessageFlags.Ephemeral,
-        });
-        return stopMiddlewares();
-    }
+  if (!serverAutoShareConfig?.enabled) {
+    (ctx.interaction as unknown as ChatInputCommandInteraction).reply({
+      content:
+        `Auto-share is not enabled on this server yet.\n` +
+        subtext(
+          `You can ask a mod or admin to enable Hevy auto-share with the ${await commandMention("server auto-share configure")} command. But be nice about it!`,
+        ),
+      flags: MessageFlags.Ephemeral,
+    });
+    return stopMiddlewares();
+  }
 }
