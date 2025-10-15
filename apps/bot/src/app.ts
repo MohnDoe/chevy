5869226324d34d1649fbfd1c0c20e838cc3cb71c
doe <bonjour@doe.cool>
2025-10-15 15:14:34 +0200

    feat: move bullMQ for building

--- apps/bot/src/app.ts
+++ apps/bot/src/app.ts
@@ -1,3 +1,6 @@
+import { setDriver } from "@commandkit/tasks";
+import { BullMQDriver } from "@commandkit/tasks/bullmq";
+import { Logger } from "commandkit";
 import { Client, GatewayIntentBits } from "discord.js";
 import dotenv from "dotenv";
 dotenv.config();
@@ -9,4 +12,16 @@ const client = new Client({
 
 client.token = process.env.DISCORD_TOKEN!;
 
+const bullMQHost = process.env.CHEVY_BULLMQ_REDIS_HOST;
+if (!bullMQHost) {
+  throw new Error("Missing environment variable: CHEVY_BULLMQ_REDIS_HOST");
+}
+
+Logger.info("Using BullMQ driver for tasks");
+setDriver(
+  new BullMQDriver({
+    host: bullMQHost,
+    port: 6379,
+  }),
+);
 export default client;
