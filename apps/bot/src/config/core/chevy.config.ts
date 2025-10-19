6e598228c7c2eff2afd2f38e4efadba5b1207ce7
doe <bonjour@doe.cool>
2025-10-19 12:14:29 +0200

    feat: add urls in description + create chevy config file

---
+++ apps/bot/src/config/core/chevy.config.ts
@@ -0,0 +1,7 @@
+const WEBSITE_DOMAIN = "chevy.fit";
+
+export default {
+  website: `https://${WEBSITE_DOMAIN}`,
+  addBotLink: `https://${WEBSITE_DOMAIN}/add-bot`,
+  mainDiscordInviteLink: `https://${WEBSITE_DOMAIN}/discord`,
+};
